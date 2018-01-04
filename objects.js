98c6af109d77751c5ec6809676cfba75ec066d29
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-01-04 19:33:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-Let playlist = new Object({ breakfast: 'oatmeal' })
+var playlist = new Object({ breakfast: 'oatmeal' })
 function updatePlaylist(playlist, artistName, songTitle) {
 
   return Object.assign({}, playlist, { [artistName]: songTitle })
