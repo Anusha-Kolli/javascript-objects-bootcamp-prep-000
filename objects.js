ad30e2ca3a8a2c26c55a99c48ce1c584bd1ca01d
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-01-04 18:51:38 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,8 @@
 var playlist = new Object({ breakfast: 'oatmeal' })
 function updatePlaylist(playlist, artistName,songTitle){
 
-   return Object.assign({}, obj, { [key]: value })
+  obj[key] = value;
+   return obj;
 
 }
 updateObjectWithKeyAndValue(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
