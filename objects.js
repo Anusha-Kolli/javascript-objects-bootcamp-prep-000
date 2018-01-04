35c479501df88029a6f23054e7302ae36dfd73be
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-01-04 19:00:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,5 +6,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
 updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi");
 
 function removeFromPlaylist(playlist, artistName){
-  return delete playlist.artistName;
+ delete playlist.artistName;
 }
